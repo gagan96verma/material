a83364e63547aed26d1f540c6c4edc571ca72593
gaganverma <gagan.verma@engineer.ai>
2020-07-24 21:15:45 +0530

    Updates

---
+++ precache-manifest.784a52ace17bf7b1592e95ada8043ccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ab626cf32679b74d20b86d36ac928f",
+    "url": "/material/index.html"
+  },
+  {
+    "revision": "6ef82c81078fb80eb525",
+    "url": "/material/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ef65e5fdb81690656e45",
+    "url": "/material/static/js/2.433bf5ae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material/static/js/2.433bf5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef82c81078fb80eb525",
+    "url": "/material/static/js/main.183c85d8.chunk.js"
+  },
+  {
+    "revision": "093c2dbcad53e7d99a7c",
+    "url": "/material/static/js/runtime-main.1d0e9c78.js"
+  }
+]);
